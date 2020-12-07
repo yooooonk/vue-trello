c20426fefd4cab39211880a0158bcaccd4351d82
ouo_yoonk <joyk9251@gmail.com>
2020-12-07 19:57:07 +0900

    api 호출

--- vue-trello/src/components/Home.vue
+++ vue-trello/src/components/Home.vue
@@ -3,6 +3,10 @@
         Home
         <div>
             Board List
+            <div v-if="loading">Loaing...</div>
+            <div v-else>
+                Api result : {{apiRes}}
+            </div>
             <ul>
                 <li><router-link to="/b/1">Board 1</router-link></li>
                 <li><router-link to="/b/2">Board 2</router-link></li>
@@ -15,7 +19,37 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
+    data(){
+        return {
+            loading : false,
+            apiRes:'',
+            error:''
+        }
+    },
+    created(){
+        this.fetchData()
+    },
+    methods:{
+        fetchData(){
+            this.loading = true
+
+            axios.get('http://localhost:3000/health')
+            .then(res=>{
+                this.apiRes = res.data
+            })
+            .catch(res=>{
+                this.error = res.response.data
+            })
+            .finally(()=>{
+                this.loading = false
+            })
+
+            
+        }
+    }
     
 }
 </script>
