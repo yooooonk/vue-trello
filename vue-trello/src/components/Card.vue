22d5c4c400456001f9d2918e77c723a3defd8d97
ouo_yoonk <joyk9251@gmail.com>
2020-12-11 16:20:54 +0900

    카드 수정

--- vue-trello/src/components/Card.vue
+++ vue-trello/src/components/Card.vue
@@ -2,15 +2,17 @@
     <Modal class="modal-card">
         <div slot="header" class="modal-card-header">
         <div class="modal-card-header-title">
-            <input class="form-control" type="text" :value="card.title" readonly>
+            <input class="form-control" type="text" ref="inputTitle" :value="card.title" 
+            :readonly="!toggleTitle" @click="toggleTitle=true"  @blur="onBlurTitle" 
+            >
         </div>
         <a class="modal-close-btn" href="" @click.prevent="onClose">&times;</a>
         </div>
         <div slot="body">
         <h3>Description</h3>
         <textarea  class="form-control" cols="30" rows="3" placeholder="Add a more detailed description..."
-            readonly
-            v-model="card.description"></textarea>
+            :readonly="!toggleDescription" @click="toggleDescription=true" @blur="onBlurDescription"
+            v-model="card.description" ref="inputDescription"></textarea>
         </div>
         <div slot="footer"></div>
     </Modal>
@@ -23,19 +25,45 @@ export default {
     components:{
         Modal
     },
+    data(){
+        return {
+            toggleTitle:false,
+            toggleDescription:false
+        }
+    },
     computed:{
         ...mapState(['card','board'])
     }, 
     methods:{
-        ...mapActions(['FETCH_CARD']),
+        ...mapActions(['FETCH_CARD','UPDATE_CARD']),
         onClose() {
              this.$router.push(`/b/${this.board.id}`)
+        },
+        fetchCard(){
+            const id = this.$route.params.cid
+            this.FETCH_CARD({id})
+        },
+        onBlurTitle(){
+            this.toggleTitle = false
+            const title = this.$refs.inputTitle.value.trim()
+            if(!title) return
+
+            this.UPDATE_CARD({id:this.card.id,title})
+                .then(()=>this.fetchCard())
+        },
+        onBlurDescription(){
+            this.toggleDescription = false
+            const description = this.$refs.inputDescription.value.trim()
+            if(!description) return
+
+              this.UPDATE_CARD({id:this.card.id,description})
+                .then(()=>this.fetchCard())
         }
     },
     created(){
-        const id = this.$route.params.cid
-        this.FETCH_CARD({id})
-    }
+           this.fetchCard()
+    },
+        
 }
 </script>
 
--- vue-trello/src/store/actions.js
+++ vue-trello/src/store/actions.js
@@ -1,4 +1,5 @@
 import * as api from '../api'
+import state from './state'
 
 const actions ={
     LOGIN({commit},{email,password}){
@@ -28,6 +29,10 @@ const actions ={
                 .then(data=>{
                     commit('SET_CARD',data.item)
                 })
+    },
+    UPDATE_CARD({dispatch,state},{id,title,description,pos,listId}){
+        return api.card.update(id,{title,description,pos,listId})
+            .then(()=>dispatch('FETCH_BOARD',{id:state.board.id}))
     }
     
 }
